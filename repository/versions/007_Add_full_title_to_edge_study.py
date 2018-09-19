a615630f6dc324527603f5c779d0a05318b8ccbe
Richard Bramley <rabramley@gmail.com>
2018-09-19 11:54:14 +0100

    Add length to nvarchar

--- repository/versions/007_Add_full_title_to_edge_study.py
+++ repository/versions/007_Add_full_title_to_edge_study.py
@@ -4,7 +4,7 @@ def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
     study = Table('edge_study', meta, autoload=True)
 
-    full_title = Column('full_title', NVARCHAR)
+    full_title = Column('full_title', NVARCHAR(500))
     full_title.create(study)
 
 def downgrade(migrate_engine):
