27d32d1e689b8789b0bc2538333bc9fc1160335d
Richard Bramley <rabramley@gmail.com>
2022-03-01 09:54:07 +0000

    Remove HIC repository

--- manage_hic_covid.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-from api.environment import HIC_COVID_CONNECTION_STRING
-
-if __name__ == '__main__':
-    main(url=HIC_COVID_CONNECTION_STRING, repository='hic_covid_repository', debug='False')
